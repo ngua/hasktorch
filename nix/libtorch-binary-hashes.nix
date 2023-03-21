6fcf5d3af441ab1b7da445ac08222b4f50793718
Rory Tyler Hayford <rory.hayford@protonmail.com>
2023-03-21 12:13:12 +0700

    Add `libtorch` version `1.13.0`

--- nix/libtorch-binary-hashes.nix
+++ nix/libtorch-binary-hashes.nix
@@ -1,4 +1,21 @@
 version : builtins.getAttr version {
+  "1.13.0" = {
+    x86_64-darwin-cpu = {
+      name = "libtorch-macos-1.13.0.zip";
+      url = "https://download.pytorch.org/libtorch/cpu/libtorch-macos-1.13.0.zip";
+      hash = "sha256-5O+/7N5flwW8yHe7Yqu0aAwtjOoo4orGWb27Qr5UXJc=";
+    };
+    x86_64-linux-cpu = {
+      name = "libtorch-cxx11-abi-shared-with-deps-1.13.0-cpu.zip";
+      url = "https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-1.13.0%2Bcpu.zip";
+      hash = "sha256-97REvbyEdoE2dhsu6IOplt4qjWV6OTMYxEqKmgUy/Yo=";
+    };
+    x86_64-linux-cuda-11 = {
+      name = "libtorch-cxx11-abi-shared-with-deps-1.13.0-cu117.zip";
+      url = "https://download.pytorch.org/libtorch/cu117/libtorch-cxx11-abi-shared-with-deps-1.13.0%2Bcu117.zip";
+      hash = "sha256-XwJnhRl0f0gT6BKUnEut1X+2mRWl1sIA6prUodFhFpc=";
+    };
+  };
   "1.11.0" = {
     x86_64-darwin-cpu = {
       name = "libtorch-macos-1.11.0.zip";
